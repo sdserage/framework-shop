59f00f9061e4561ece172f51409aaa96f58e6614
Scott Serage <xttocsurge51@gmail.com>
2017-09-19 14:31:42 -0600

    Completed Step 6

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -5,12 +5,26 @@ import "./Shop.css";
 
 import { addToCart } from "../../ducks/product";
 
+// Components
+import ProductTile from './ProductTile/ProductTile';
+
 export function Shop( { addToCart, products } ) {
+	let productTiles = products.map(product=>{
+		return(
+			<ProductTile
+				addToCart={()=> addToCart(product.id)}
+				key={product.id}
+				logo={product.logo}
+				name={product.name}
+				price={product.price}
+			/>
+		);
+	});
 	return (
 		<div className="shop">
 			<h1 className="shop__header">Shop</h1>
 			<div className="shop__products-wrapper">
-
+				{productTiles}
 			</div>
 		</div>
 	);
