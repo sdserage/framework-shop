665e38dfbd2d7990d6bb85cb30b4040559a17b5e
Scott Serage <xttocsurge51@gmail.com>
2017-09-19 14:49:15 -0600

    Completed Final Step (Step 7, no 'Black Diamond').

--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import Landing from './components/Landing/Landing';
 import Shop from './components/Shop/Shop';
 import Details from './components/Details/Details';
 import Cart from './components/Cart/Cart';
+import ThankYou from './components/ThankYou/ThankYou';
 
 export default (
   <Switch>
@@ -12,5 +13,6 @@ export default (
     <Route path='/shop' component={Shop}/>
     <Route path='/details/:name' component={Details}/>
     <Route path='/cart' component={Cart}/>
+    <Route path='/thank-you' component={ThankYou}/>
   </Switch>
 );
